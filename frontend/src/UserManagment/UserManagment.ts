dfa9576bf2c851f816cb9d5138e052b017a96ea9
Kipras-Zelenkovas <80096515+Kipras-Zelenkovas@users.noreply.github.com>
2022-08-05 00:00:19 +0300

    Backend calling functions
    Added axios.
    Added main backend calling functions.
    UserManagment: getUsers, getUser, createUser, updateUser, deleteUser.

--- frontend/src/UserManagment/UserManagment.ts
+++ frontend/src/UserManagment/UserManagment.ts
@@ -1,5 +1,64 @@
-const GetUsers = () => {
-    return 0;
+import axios from "axios"
+
+const getUsers = () => {
+    let data = axios.get('https://adminpanel.ddev.site/api/user/')
+                    .then((res) => {
+                        return res.data
+                    }).catch((error) => {
+                        return error.data
+                    })
+
+    return data
 }
 
-export default {GetUsers}
+const getUser = (id: number) => {
+    let data = axios.get('https://adminpanel.ddev.site/api/user/' + id)
+                    .then((res) => {
+                        return res.data
+                    }).catch((error) => {
+                        return error.data
+                    })
+
+    return data
+}
+
+const createUser = (name: string, email: string, password: string) => {
+    let data = axios.post('https://adminpanel.ddev.site/api/user', {
+                        name: name,
+                        email: email,
+                        password: password
+                    }).then((res) => {
+                        return res.data
+                    }).catch((error) => {
+                        return error.data
+                    })
+
+    return data
+}
+
+const updateUser = (id: number, name: string, email: string, password: string) => {
+    let data = axios.put('https://adminpanel.ddev.site/api/user/' + id, {
+                        name: name,
+                        email: email,
+                        password: password
+                    }).then((res) => {
+                        return res.data
+                    }).catch((error) => {
+                        return error.data
+                    })
+
+    return data
+}
+
+const deleteUser = (id: number) => {
+    let data = axios.delete('https://adminpanel.ddev.site/api/user/' + id)
+                    .then((res) => {
+                        return res.data
+                    }).catch((error) => {
+                        return error.data
+                    })
+
+    return data
+}
+
+export {getUsers, getUser, createUser, updateUser, deleteUser}
