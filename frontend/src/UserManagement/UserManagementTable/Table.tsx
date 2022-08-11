2159710230d005099c689ee97e643c00d32a95ac
Kipras-Zelenkovas <80096515+Kipras-Zelenkovas@users.noreply.github.com>
2022-08-11 22:06:48 +0300

    Cancel HTTP request
    Added axios cancelToken, when going to the next or previous page, it cancels http request.

--- frontend/src/UserManagement/UserManagementTable/Table.tsx
+++ frontend/src/UserManagement/UserManagementTable/Table.tsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react"
 import { UsersData } from "../../utils/Interfaces"
 import { getUsers } from "../UserManagment"
+import axios from "axios"
 
 const UserManagementTable = () => {
     
     const [data, setData] = useState<UsersData[]>([])
 
     useEffect(() => {
-        getUsers(setData)
+        const cancelToken = axios.CancelToken.source()
+        getUsers(setData, cancelToken)
+
+        return () => {
+            cancelToken.cancel()
+        }
     }, [])
     
     if(data.length === 0){
--- frontend/src/UserManagement/UserManagment.ts
+++ frontend/src/UserManagement/UserManagment.ts
@@ -1,57 +1,77 @@
-import axios from "axios"
+import axios, { CancelTokenSource } from "axios"
 import { UsersChangeableData } from "../utils/Interfaces"
 
-const getUsers = (setData: Function) => {
-    axios.get('https://adminpanel.ddev.site/api/user/')
+const getUsers = (setData: Function, cancelToken: CancelTokenSource) => {
+    axios.get('https://adminpanel.ddev.site/api/user/', {cancelToken: cancelToken.token})
         .then((res) => {
             console.log(res.data)
             setData(res.data)
-        }).catch((error) => {
-            setData(error)
+        }).catch((err) => {
+            if(axios.isCancel(err)){
+                console.log('Canceled')
+            }else{
+                console.log(err)
+            }   
         })
 }
 
-const getUser = (id: number, setData: Function) => {
-    axios.get('https://adminpanel.ddev.site/api/user/' + id)
+const getUser = (id: number, setData: Function, cancelToken: CancelTokenSource) => {
+    axios.get('https://adminpanel.ddev.site/api/user/' + id, {cancelToken: cancelToken.token})
         .then((res) => {
             console.log(res.data)
             setData(res.data)
-        }).catch((error) => {
-            setData(error)
+        }).catch((err) => {
+            if(axios.isCancel(err)){
+                console.log('Canceled')
+            }else{
+                console.log(err)
+            }
         })
 }
 
-const createUser = (users_data: UsersChangeableData) => {
+const createUser = (users_data: UsersChangeableData, cancelToken: CancelTokenSource) => {
     axios.post('https://adminpanel.ddev.site/api/user', {
             name: users_data.name,
             email: users_data.email,
             password: users_data.password
-        }).then((res) => {
+        }, {cancelToken: cancelToken.token}).then((res) => {
             return res.data
-        }).catch((error) => {
-            return error.data
+        }).catch((err) => {
+            if(axios.isCancel(err)){
+                console.log('Canceled')
+            }else{
+                console.log(err)
+            }
         })
 }
 
-const updateUser = (id: number, users_data: UsersChangeableData) => {
+const updateUser = (id: number, users_data: UsersChangeableData, cancelToken: CancelTokenSource) => {
     axios.put('https://adminpanel.ddev.site/api/user/' + id, {
             name: users_data.name,
             email: users_data.email,
             password: users_data.password
-        }).then((res) => {
+        }, {cancelToken: cancelToken.token}).then((res) => {
             return res.data
-        }).catch((error) => {
-            return error.data
+        }).catch((err) => {
+            if(axios.isCancel(err)){
+                console.log('Canceled')
+            }else{
+                console.log(err)
+            }
         })
 
 }
 
-const deleteUser = (id: number) => {
-    axios.delete('https://adminpanel.ddev.site/api/user/' + id)
+const deleteUser = (id: number, cancelToken: CancelTokenSource) => {
+    axios.delete('https://adminpanel.ddev.site/api/user/' + id, {cancelToken: cancelToken.token})
         .then((res) => {
             return res.data
-        }).catch((error) => {
-            return error.data
+        }).catch((err) => {
+            if(axios.isCancel(err)){
+                console.log('Canceled')
+            }else{
+                console.log(err)
+            }
         })
 }
 
